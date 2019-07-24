56d3ae9c99f5e367a48de9ec33ce0bf19fdf0b54
Becca Hallam <46824047+rch16@users.noreply.github.com>
2019-07-24 10:56:17 +0100

    Adding comments

--- Eliot FYP/matlab_serialcomm.m
+++ Eliot FYP/matlab_serialcomm.m
@@ -1,3 +1,6 @@
+% Create a serial port value on x and takes in properties of the object as arguments
+% USB port = /dev/cu.usmodem14101
+% BAUD of the object = 9600
 x = serial('/dev/cu.usbmodem14101', 'BAUD', 9600);
 
 fopen(x);
@@ -8,8 +11,10 @@ while go
     a = input('Input the number of connected terminals, Press 0 to break: ');
     
     if (a == 0)
+        % If user inputs 0, choreography finishes
         go = false;
     else
+        % Formats input as a single character
         fprintf(x, '%c', a);
     end
 end
