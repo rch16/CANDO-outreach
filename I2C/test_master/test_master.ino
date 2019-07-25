c39a53afde41016f635136e1716adde178616cc7
Becca Hallam <rch16@ic.ac.uk>
2019-07-25 16:20:45 +0100

    adding comments

--- I2C/test_master/test_master.ino
+++ I2C/test_master/test_master.ino
@@ -4,30 +4,36 @@
 
 void setup()
 {
+  // begin i2c communication - address optional for master
   Wire.begin(); 
+  // open a serial channel
   Serial.begin(9600);
 }
 
 void loop()
 {
+  // transmit to slave 1, H = HIGH
   Wire.beginTransmission(1);
   Wire.write('H');
   Wire.endTransmission();
  
   delay(500);
   
+  // transmit to slave 1, L = LOW
   Wire.beginTransmission(1);
   Wire.write('L');
   Wire.endTransmission();
  
   delay(500);
   
+  // transmit to slave 2, H = HIGH
   Wire.beginTransmission(2);
   Wire.write('H');
   Wire.endTransmission();
  
   delay(500);
   
+  // transmit to slave 2, L = LOW
   Wire.beginTransmission(2);
   Wire.write('L');
   Wire.endTransmission();
--- I2C/test_slave_2/test_slave_2.ino
+++ I2C/test_slave_2/test_slave_2.ino
@@ -1,4 +1,4 @@
-// test slave i2c
+// test slave i2c - arduino 2
 
 #include <Wire.h>
 
@@ -6,7 +6,9 @@ const byte slaveId = 2;
 
 void setup()
 {
+  // begin i2c communication at relevant address
   Wire.begin(slaveId); 
+  // if data received, call receiveEvent function
   Wire.onReceive(receiveEvent);
  
   pinMode(LED_BUILTIN,OUTPUT);
@@ -19,18 +21,22 @@ void loop()
 
 void receiveEvent(int howMany)
 {
+  // communication received
   char inChar;
   
   while(Wire.available() > 0)
   {
+    // read data
     inChar = Wire.read();  
     
     if (inChar == 'H')
     {
+      // LED high
       digitalWrite(LED_BUILTIN, HIGH);
     }
     else if (inChar == 'L')
     {
+      // LED low
       digitalWrite(LED_BUILTIN, LOW);
     }
   }
