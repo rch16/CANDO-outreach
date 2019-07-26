06394bd82c6aec9d3ff6640ca3b8e9dc8b4e8bba
Becca Hallam <rch16@ic.ac.uk>
2019-07-26 17:19:01 +0100

    implementing user serial control via matlab

--- I2C/matlab_control.m
+++ I2C/matlab_control.m
@@ -1,5 +1,5 @@
 % Create a serial port value on x and takes in properties of the object as arguments
-% USB port = /dev/cu.usmodem14101
+% USB port = /dev/cu.usmodem14201
 % BAUD of the object = 9600
 x = serial('/dev/cu.usbmodem14201', 'BAUD', 9600);
 
@@ -8,16 +8,20 @@ fopen(x);
 go = true;
 
 while go
-    a = input('Input "go" to start motor, Input "stop" to finish: ');
+    a = input('Input 1 to start motor, Input 0 to finish: ');
     
-    if (a == "stop")
+    if (a == 0)
         % If user inputs "stop", motor stops
-        go = false;
-    else if (a == "go")
+        start = false;
+        fprintf('stopping motor\n');
+        break;
+    else if (a == 1)
         % If user inputs "go", motor starts
-        fprintf("Starting motor", '%s');
+        start = true;
+        fprintf('starting motor\n');
     else
         % Invalid input, do nothing
+    end
     end
 end
 
