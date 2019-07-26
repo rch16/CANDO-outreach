d8e27700566e9289d01df2ed74116d0a34117016
Becca Hallam <rch16@ic.ac.uk>
2019-07-26 17:23:00 +0100

    testing serial matlab control

--- I2C/test_master/test_master.ino
+++ I2C/test_master/test_master.ino
@@ -2,6 +2,8 @@
 
 #include <Wire.h>
 
+int ready = 0;
+
 void setup()
 {
   // begin i2c communication - address optional for master
@@ -12,31 +14,39 @@ void setup()
 
 void loop()
 {
-  // transmit to slave 1, H = HIGH
-  Wire.beginTransmission(1);
-  Wire.write('H');
-  Wire.endTransmission();
- 
-  delay(500);
+  While(ready == 1){
+    // transmit to slave 1, H = HIGH
+    Wire.beginTransmission(1);
+    Wire.write('H');
+    Wire.endTransmission();
   
-  // transmit to slave 1, L = LOW
-  Wire.beginTransmission(1);
-  Wire.write('L');
-  Wire.endTransmission();
- 
-  delay(500);
+    delay(500);
+    
+    // transmit to slave 1, L = LOW
+    Wire.beginTransmission(1);
+    Wire.write('L');
+    Wire.endTransmission();
   
-  // transmit to slave 2, H = HIGH
-  Wire.beginTransmission(2);
-  Wire.write('H');
-  Wire.endTransmission();
- 
-  delay(500);
+    delay(500);
+    
+    // transmit to slave 2, H = HIGH
+    Wire.beginTransmission(2);
+    Wire.write('H');
+    Wire.endTransmission();
   
-  // transmit to slave 2, L = LOW
-  Wire.beginTransmission(2);
-  Wire.write('L');
-  Wire.endTransmission();
- 
-  delay(500);
+    delay(500);
+    
+    // transmit to slave 2, L = LOW
+    Wire.beginTransmission(2);
+    Wire.write('L');
+    Wire.endTransmission();
+  
+    delay(500);
+  }
 }
+
+// Serial data coming from the PC interfae currently comes in the form of a single integer
+// This represents the amount of terminals connected to the network
+void serialEvent() {
+  ready = Serial.read(); // Ready to start or not? 0 = NO, 1 = YES
+  }
