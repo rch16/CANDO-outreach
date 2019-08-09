bb489ea81d5951900e7a5f2c41a66c2b381a77ec
Becca Hallam <rch16@ic.ac.uk>
2019-08-09 17:17:18 +0100

    removing implementation of user inputted number of slaves

--- I2C/GUI.m
+++ I2C/GUI.m
@@ -100,28 +100,3 @@ catch
    errordlg('Port not disconnected.', 'Disconnection Error');
 end
 
-
-% --- Executes on selection change in popupmenu1.
-function popupmenu1_Callback(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    structure with handles and user data (see GUIDATA)
-global s;
-contents = cellstr(get(hObject,'String'));
-x = contents{get(hObject,'Value')};
-fprintf(s, '%d', x);
-% Hints: contents = cellstr(get(hObject,'String')) returns popupmenu1 contents as cell array
-%        contents{get(hObject,'Value')} returns selected item from popupmenu1
-
-
-% --- Executes during object creation, after setting all properties.
-function popupmenu1_CreateFcn(hObject, eventdata, handles)
-% hObject    handle to popupmenu1 (see GCBO)
-% eventdata  reserved - to be defined in a future version of MATLAB
-% handles    empty - handles not created until after all CreateFcns called
-
-% Hint: popupmenu controls usually have a white background on Windows.
-%       See ISPC and COMPUTER.
-if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
-    set(hObject,'BackgroundColor','white');
-end
