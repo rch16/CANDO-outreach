0a4bca7325bb97fe65cfc16480395f7149039a04
Becca Hallam <rch16@ic.ac.uk>
2019-08-09 12:18:56 +0100

    simple working i2c communication

--- I2C/slave/slave.ino
+++ I2C/slave/slave.ino
@@ -1,43 +1,37 @@
 // slave arduino motor control
 
-// I2C communication sends:
-// Number of Steps = 6240
-// Max speed = 600
-// Iterations = 10
-
 #include <Wire.h>
 
-// - - - - - - - - - PINS - - - - - - - - - 
+// Which slave?
+// This address has to match up to where the Master is sending information
+const byte slaveID = 1; // change depending on which arduino you're sending code to
+
+// Pins
 byte directionPin = 9;
 byte stepPin = 8;
 byte ledPin = 13;
 
-// - - - - - - - - - SLAVE DATA - - - - - - - - - 
-// Slave address has to match up to where the Master is sending information
-const byte slaveID = 1; // change depending on which arduino you're sending code to
-
-// - - - - - - - - - MOTOR INITIALISATION - - - - - - - - - 
-
-const int exponential_steps = 50;     //Number of steps in the exponential stages
-const int exponential_increment = 10;   //How much the speed will have changed by after exponential_steps
+// Set values
+const int arrsz = 50;     //Number of steps it takes for ∂f/∂x to reach 1 
+const int exp_inc = 10;   //what speed value is reached after arrsz steps 
 const int minspeed = 2500; //1000; // delay, not speed
-int maxspeed = 1000; //300;
+
 int numberOfSteps = 1125; //ONLY WORKS WITH NBOFSTEPS > 1524 -> incorrect value?
 int iterations = 1;
+int maxspeed = 1000; //300;
 int motorSpeed = 0;
 
-// - - - - - - - - - MOTOR OPERATION ARRAYS - - - - - - - - - 
-int Sa[exponential_steps];   //Array starting at low speed
-int Sb[exponential_steps];   //Array finishing at high speed
-int Sc[2*exponential_steps]; //Extended array starting at low speed
-int temp[exponential_steps]; //Temp array
+// Motor operation arrays
+int Sa[arrsz];   //Array starting at low speed
+int Sb[arrsz];   //Array finishing at high speed
+int Sc[2*arrsz]; //Extended array starting at low speed
+int temp[arrsz]; //Temp array
 int time_array[6]; //tn determine the steps at which we switch between linear/exponential growth
 
-// - - - - - - - - - I2C COMMUNICATION - - - - - - - - - 
+// Changed using i2c
 bool go = false; 
 int input = false;
-
-// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+  
 void setup() 
 { 
   Serial.begin(9600);
@@ -53,106 +47,53 @@ void setup()
   digitalWrite(directionPin, HIGH); // start with direction pin high
   // -> Pulley goes down first
 
-  create_arrays(); // creating arrays
-  //time_array_mod(time_array); // assigning time array
-  time_array_mod(time_array, exponential_increment, maxspeed, exponential_steps, minspeed, numberOfSteps);
+  createArrays(); // creating arrays
+  time_array_mod(time_array); // assigning time array
 }
 
 void loop() { 
   int d = digitalRead(directionPin);
   
-  /*if(go){
-    decode_transmission();
-    go = false;
+  if(go == true){ // if changed to false, stops motor at end of current motion loop
     
     for(int i = 0; i < iterations; i++){
-      change_direction(d);
+      changeDirection(d);
       motorSpeed = minspeed;
       for (int i = 0; i < numberOfSteps; i++){
         digitalWrite(stepPin, HIGH);
         delayMicroseconds(motorSpeed + 4);
         digitalWrite(stepPin, LOW);
         delayMicroseconds(motorSpeed);
-        motorSpeed = step_interval(i, time_array);
+        motorSpeed = stepInterval(i, time_array);
         //Serial.println(motorSpeed);
         }
       delayMicroseconds(100);
     }
-  }*/
-  if (go){
-    decode_transmission();
-    go =  false;
-
-    for(int i = 0; i < iterations; i++){
-      digitalWrite(directionPin, HIGH);
-      motorSpeed = minspeed;
-      for (int i = 0; i < numberOfSteps; i++){
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(motorSpeed + 4);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(motorSpeed);
-        motorSpeed = step_interval(i, time_array);
-        //Serial.println(motorSpeed);
-        }
-      delayMicroseconds(4);
-    
-      digitalWrite(directionPin, LOW);
-      motorSpeed = minspeed;
-      for (int i = 0; i < numberOfSteps; i++){
-        digitalWrite(stepPin, HIGH);
-        delayMicroseconds(motorSpeed + 4);
-        digitalWrite(stepPin, LOW);
-        delayMicroseconds(motorSpeed);
-        motorSpeed = step_interval(i, time_array);
-      }
-    }
-  digitalWrite(LED_BUILTIN, LOW); // motor control complete
   }
+  digitalWrite(LED_BUILTIN, LOW);
 }
 
-void decode_transmission(){
-  // decode the data sent via I2C using bit shifts
-    Serial.println("config changes");
-    numberOfSteps = Wire.read();
-    numberOfSteps += Wire.read() << 4;
-    numberOfSteps += Wire.read() << 8;
-    numberOfSteps += Wire.read() << 12;
-    Serial.print("Number of Steps = ");
-    Serial.println(numberOfSteps);
-
-    maxspeed = Wire.read()*10;
-    Serial.print("Max speed = ");
-    Serial.println(maxspeed);
-
-    iterations = Wire.read();
-    Serial.print("Iterations = ");
-    Serial.println(iterations);
-
-    time_array_mod(time_array, exponential_increment, maxspeed, exponential_steps, minspeed, numberOfSteps);
-    exp_array_mod(exponential_steps, maxspeed, Sb);
-}
-
-void create_arrays(){
+void createArrays(){
   //USING L/1+exp(-k(x-mid))
   // L = curve's maximum value = 500
   // k = steepness of curve = 0.1
   int mid = 20; // mid-point x-value of curve
-  for(int i = 0; i < exponential_steps; i++){
+  for(int i = 0; i < arrsz; i++){
     Sa[i] = round(-500/(1+exp(-0.1*((i+1)-mid))) + minspeed);
     //Serial.println(Sa[i]);
-    Sb[i] = round(-500/(1+exp(-0.1*(160-(exponential_steps-i) - 130))) + (maxspeed+480));
+    Sb[i] = round(-500/(1+exp(-0.1*(160-(arrsz-i) - 130))) + (maxspeed+480));
     //Serial.println(Sb[i]);
     temp[i] = Sa[i] - 411;
     }
-  for(int j = 0; j < exponential_steps; j++){
+  for(int j = 0; j < arrsz; j++){
     Sc[j] = Sa[j];
   }
-  for(int k = exponential_steps; k < 2*exponential_steps; k++){
-    Sc[k] = temp[k - exponential_steps];
+  for(int k = arrsz; k < 2*arrsz; k++){
+    Sc[k] = temp[k - arrsz];
   }
 }
 
-void change_direction(int d){
+void changeDirection(int d){
   if(d == 0){
     digitalWrite(directionPin, HIGH);
   }
@@ -161,7 +102,7 @@ void change_direction(int d){
   }
 }
 //Sets speed depending on step interval in which we are
-int step_interval(int i, int t[]){
+int stepInterval(int i, int t[]){
   if (i < t[0]){    //1st exponential (slow) increase from minspeed
     motorSpeed = Sa[i];
     //Serial.print("one = ");
@@ -179,7 +120,7 @@ int step_interval(int i, int t[]){
     //Serial.print("four = ");
   }
   else if(i > t[3] && i < t[4]){    //1st exponential decay from maxspeed
-    motorSpeed = Sb[(exponential_steps-1)-(i-t[3])];
+    motorSpeed = Sb[(arrsz-1)-(i-t[3])];
     //Serial.print("five = ");
   }
   else if(i >= t[4] && i < t[5]){   //linear decrease
@@ -187,38 +128,35 @@ int step_interval(int i, int t[]){
     //Serial.print("six = ");
   }
   else if(i >= t[5]){   //2nd exponential decay until minspeed
-    motorSpeed = Sc[(2*exponential_steps-1)-(i-t[5])] + 76;
+    motorSpeed = Sc[(2*arrsz-1)-(i-t[5])] + 76;
     //Serial.print("seven = ");
   }
   //Serial.println(motorSpeed);
   return motorSpeed;
  }
 
-void time_array_mod(int t[], int exponential_increment, int maxspeed, int exponential_steps, int minspeed, int numberOfSteps){
-  t[0] = exponential_steps;
-  t[1] = exponential_steps + (minspeed-maxspeed) - 2*exponential_increment;
-  t[2] = time_array[1] + exponential_steps;
-  t[5] = numberOfSteps - exponential_steps;
-  t[4] = t[5] - (minspeed-maxspeed - 2*exponential_increment) - 1;
-  t[3] = t[4] - exponential_steps;
+void time_array_mod(int t[]){
   // time intervals for different stages in the acceleration/deceleration curve
-  /*t[0] = exponential_steps;
-  t[1] = exponential_steps + minspeed/4 - 2*exponential_increment; 
-  t[2] = t[1] + exponential_steps;
+  t[0] = arrsz;
+  t[1] = arrsz + minspeed/4 - 2*exp_inc; 
+  t[2] = t[1] + arrsz;
   t[3] = t[2];
-  t[4] = t[3] + exponential_steps;
-  t[5] = numberOfSteps - 2*exponential_steps;*/
+  t[4] = t[3] + arrsz;
+  t[5] = numberOfSteps - 2*arrsz;
 }
-
- void exp_array_mod(int exponential_steps, int maxspeed, int Sb[]){
-  // modifiy the exponential motion array
-  for(int i = 0; i < exponential_steps; i++){
-    Sb[i] = round(-500/(1+exp(-0.1*(160-(exponential_steps-i) - 80))) + (500+maxspeed));
-    }
-  }
 
 void receiveEvent(int bytes){
   // communication received
-  digitalWrite(LED_BUILTIN, HIGH);// LED high
-  go = true;
+  while(Wire.available() > 0)
+  { 
+    input = Wire.read();  
+    if (input == 1){ // read data
+      digitalWrite(LED_BUILTIN, HIGH);// LED high
+      go = true; // start motor!
+    }
+    else if (input == 0){
+      digitalWrite(LED_BUILTIN, LOW); // LED low
+      go = false; // stop motion
+    }
+  }
 }
